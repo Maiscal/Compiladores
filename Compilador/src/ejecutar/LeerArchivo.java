a0446f797bbbd1bd73a07a782ef45c1ff6d0b8a9
Isaac <38108707+Maiscal@users.noreply.github.com>
2023-05-25 11:52:23 -0500

    Add files via upload

--- Compilador/src/ejecutar/LeerArchivo.java
+++ Compilador/src/ejecutar/LeerArchivo.java
@@ -16,10 +16,36 @@ public class LeerArchivo {
     
   
     public static void main(String[] args) {
+        /*
+        String rutaArchivo = "ruta_del_archivo.txt";
+
+        try {
+            // Crea una instancia de la clase File
+            File archivo = new File(rutaArchivo);
+
+            // Crea una instancia de la clase Scanner para leer el archivo
+            Scanner scanner = new Scanner(archivo);
+
+            // Lee el archivo línea por línea
+            while (scanner.hasNextLine()) {
+                String linea = scanner.nextLine();
+                System.out.println(linea);
+            }
+
+            // Cierra el Scanner
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("El archivo no pudo ser encontrado.");
+            e.printStackTrace();
+        }
+    }
+}
+        */
         
-         String input = "int adsqwe = 54;";
+        
+         String input = "string adsqwe = 54 + 50;";
         /*Definir patrones de tokens*/ 
-        Pattern keywordPattern = Pattern.compile("\\b(int|float|boolean)\\b");
+        Pattern keywordPattern = Pattern.compile("\\b(int|float|string)\\b");
         Pattern identifierPattern = Pattern.compile("\\b[a-zA-Z][a-zA-Z0-9]*\\b");
         Pattern operatorPattern = Pattern.compile("[=+\\-*/]");
 
